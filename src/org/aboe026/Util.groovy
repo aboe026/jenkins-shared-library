4078c1151385ac47da8463c7acb6fcd9242ba7c1
Adam Boe <adamboe@outlook.com>
2022-03-02 21:13:09 -0800

    debug

--- src/org/aboe026/Util.groovy
+++ src/org/aboe026/Util.groovy
@@ -9,6 +9,8 @@ class Util {
     @NonCPS
     static String extractMethodName() {
         Throwable marker = new Throwable()
+        println 'TEST extractMethodName StackTraceUtils.sanitize(marker):'
+        println StackTraceUtils.sanitize(marker)
         return StackTraceUtils.sanitize(marker).stackTrace[1].methodName
     }
 
