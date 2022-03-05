ee3f066fa97e3b3518af7820bb385b6fbfe59261
Adam Boe <adamboe@outlook.com>
2022-03-05 12:40:39 -0800

    color enum

--- src/org/aboe026/ShieldsIoBadges.groovy
+++ src/org/aboe026/ShieldsIoBadges.groovy
@@ -120,22 +120,22 @@ class ShieldsIoBadges implements Serializable {
         this.steps.println "TEST before switch"
         switch (percentage) {
             case 100:
-                color = 'brightgreen' // color = Color.BRIGHT_GREEN
+                color = Color.BRIGHT_GREEN
                 break
             case 90..100:
-                color = 'green' // color = Color.GREEN
+                color = Color.GREEN
                 break
             case 80..90:
-                color = 'yellowgreen' // color = Color.YELLOW_GREEN
+                color = Color.YELLOW_GREEN
                 break
             case 70..80:
-                color = 'yellow' // color = Color.YELLOW
+                color = Color.YELLOW
                 break
             case 60..70:
-                color = 'orange' // color = Color.ORANGE
+                color = Color.ORANGE
                 break
             default:
-                color = 'red' // color = Color.RED
+                color = Color.RED
                 break
         }
         this.steps.println 'TEST after switch'
@@ -156,78 +156,78 @@ class ShieldsIoBadges implements Serializable {
 
 }
 
-// enum Color {
+enum Color {
 
-//   BRIGHT_GREEN {
+  BRIGHT_GREEN {
 
-//     @Override
-//     String toString() {
-//       return 'brightgreen'
-//     }
+    @Override
+    String toString() {
+      return 'brightgreen'
+    }
 
-//   },
+  },
 
-//   GREEN {
+  GREEN {
 
-//     @Override
-//     String toString() {
-//       return 'green'
-//     }
+    @Override
+    String toString() {
+      return 'green'
+    }
 
-//   },
+  },
 
-//   YELLOW_GREEN {
+  YELLOW_GREEN {
 
-//     @Override
-//     String toString() {
-//       return 'yellowgreen'
-//     }
+    @Override
+    String toString() {
+      return 'yellowgreen'
+    }
 
-//   },
+  },
 
-//   YELLOW {
+  YELLOW {
 
-//     @Override
-//     String toString() {
-//       return 'yellow'
-//     }
+    @Override
+    String toString() {
+      return 'yellow'
+    }
 
-//   },
+  },
 
-//   ORANGE {
+  ORANGE {
 
-//     @Override
-//     String toString() {
-//       return 'orange'
-//     }
+    @Override
+    String toString() {
+      return 'orange'
+    }
 
-//   },
+  },
 
-//   RED {
+  RED {
 
-//     @Override
-//     String toString() {
-//       return 'red'
-//     }
+    @Override
+    String toString() {
+      return 'red'
+    }
 
-//   },
+  },
 
-//   BLUE {
+  BLUE {
 
-//     @Override
-//     String toString() {
-//       return 'blue'
-//     }
+    @Override
+    String toString() {
+      return 'blue'
+    }
 
-//   },
+  },
 
-//   LIGHT_GREY {
+  LIGHT_GREY {
 
-//     @Override
-//     String toString() {
-//       return 'lightgrey'
-//     }
+    @Override
+    String toString() {
+      return 'lightgrey'
+    }
 
-//   }
+  }
 
-// }
+}
