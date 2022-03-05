a294de0f7e515b1ac6447d7d062397e8807f9c62
Adam Boe <adamboe@outlook.com>
2022-03-04 18:44:45 -0800

    more debug

--- src/org/aboe026/Util.groovy
+++ src/org/aboe026/Util.groovy
@@ -10,7 +10,9 @@ class Util {
     static String getMethodName(def steps) {
         String methodName = ''
         StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace()
+        steps.println("TEST stacktrace.length: '${stacktrace.length}'")
         for (int i = 0; i < stacktrace.length; i++) {
+            steps.println "TEST i: '${i}'"
             steps.println "TEST stacktrace[i].getMethodName(): '${stacktrace[i].getMethodName()}'"
             if (stacktrace[i].getMethodName() == 'getMethodName') {
                 methodName = stacktrace[i + 1].getMethodName()
