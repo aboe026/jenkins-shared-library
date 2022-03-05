1a57c7889309887a8fa4a7c5d92b52cb8991070f
Adam Boe <adamboe@outlook.com>
2022-03-04 18:48:00 -0800

    tes

--- src/org/aboe026/Util.groovy
+++ src/org/aboe026/Util.groovy
@@ -8,18 +8,12 @@ class Util {
 
     @NonCPS
     static String getMethodName(def steps) {
-        String methodName = ''
-        StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace()
-        steps.println("TEST stacktrace.length: '${stacktrace.length}'")
-        for (int i = 0; i < stacktrace.length; i++) {
-            steps.println "TEST i: '${i}'"
-            steps.println "TEST stacktrace[i].getMethodName(): '${stacktrace[i].getMethodName()}'"
-            if (stacktrace[i].getMethodName() == 'getMethodName') {
-                methodName = stacktrace[i + 1].getMethodName()
-                // break
-            }
+        def temp = Thread.currentThread().getStackTrace()
+        temp.each { tem ->
+            steps.println "TEST tem.getMethodName: '${tem.getMethodName()}'"
+            [3].getMethodName())
         }
-        return methodName
+        return ''
     }
 
 }
