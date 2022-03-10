494286adf2f4997f60682fe6073037b43282a826
Adam Boe <adamboe@outlook.com>
2022-03-09 21:31:50 -0800

    get canonical name

--- src/org/aboe026/ShieldsIoBadges.groovy
+++ src/org/aboe026/ShieldsIoBadges.groovy
@@ -27,7 +27,7 @@ class ShieldsIoBadges implements Serializable {
      *      def badges = new ShieldsIoBadges(this)
      */
     ShieldsIoBadges(Script steps) {
-        steps.println "TEST steps.getClass().getName(): '${steps.getClass().getSimpleName()}'"
+        steps.println "TEST steps.getClass().getName(): '${steps.getClass().getCanonicalName()}'"
         if (steps == null) {
             throw new Exception('Invalid parameter "null" passed to "ShieldsIoBadges" constructor: Must be non-null Script object.')
         }
@@ -44,7 +44,7 @@ class ShieldsIoBadges implements Serializable {
      *      def badges = new ShieldsIoBadges(this, '/path/in/jenkins/to/set-badge-result-job')
      */
     ShieldsIoBadges(Script steps, String setBadgeResultsJob) {
-        steps.println "TEST steps.getClass().getName(): '${steps.getClass().getSimpleName()}'"
+        steps.println "TEST steps.getClass().getName(): '${steps.getClass().getCanonicalName()}'"
         if (steps == null) {
             throw new Exception('Invalid first parameter "null" passed to "ShieldsIoBadges" constructor: Must be non-null Script object.')
         }
@@ -66,7 +66,7 @@ class ShieldsIoBadges implements Serializable {
     ShieldsIoBadges(Map params) {
         ParameterValidator.required(params, 'ShieldsIoBadges', 'steps', true)
         this.steps = params.steps
-        steps.println "TEST this.steps.getClass().getName(): '${this.steps.getClass().getSimpleName()}'"
+        steps.println "TEST this.steps.getClass().getName(): '${this.steps.getClass().getCanonicalName()}'"
         this.setBadgeResultsJob = ParameterValidator.defaultIfNotSet(params, 'setBadgeResultsJob', '/shields.io-badge-results/set-badge-result')
     }
 
