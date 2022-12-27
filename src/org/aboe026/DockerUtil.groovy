56e55fe346146db58fd754b9cb755cf2fc3bc180
Adam Boe <adamboe@outlook.com>
2022-12-26 16:45:12 -0800

    rename to docker util

--- src/org/aboe026/DockerUtil.groovy
+++ src/org/aboe026/DockerUtil.groovy
@@ -2,27 +2,27 @@ import net.sf.json.JSONObject
 
 /** Utility for interacting with Docker
   */
-class Docker implements Serializable {
+class DockerUtil implements Serializable {
 
     private static final long serialVersionUID = 1L
     private final Script steps
 
     // Not allowed, need script steps for method executions
-    Docker() {
-        throw new Exception('Empty constructor not valid for Docker class. Must pass script steps (aka "this") as either a parameter (this) or in a Map (steps: this).')
+    DockerUtil() {
+        throw new Exception('Empty constructor not valid for DockerUtil class. Must pass script steps (aka "this") as either a parameter (this) or in a Map (steps: this).')
     }
 
     /** Can be instantiated in a Jenkinsfile like:
      *
      *      @Library('aboe026') _ // groovylint-disable-line VariableName, UnusedVariable
      *
-     *      import org.aboe026.Docker
+     *      import org.aboe026.DockerUtil
      *
-     *      def docker = new Docker(this)
+     *      def dockerUtil = new DockerUtil(this)
      */
-    Docker(Script steps) {
+    DockerUtil(Script steps) {
         if (!steps) {
-            throw new Exception("Invalid first parameter \"${steps}\" passed to \"Docker\" constructor: Must be non-null Script object.")
+            throw new Exception("Invalid first parameter \"${steps}\" passed to \"DockerUtil\" constructor: Must be non-null Script object.")
         }
         this.steps = steps
     }
@@ -31,21 +31,21 @@ class Docker implements Serializable {
      *
      *      @Library('aboe026') _ // groovylint-disable-line VariableName, UnusedVariable
      *
-     *      import org.aboe026.Docker
+     *      import org.aboe026.DockerUtil
      *
-     *      def docker = new Docker(
+     *      def dockerUtil = new DockerUtil(
      *          steps: this
      *      )
      */
-    Docker(Map params) {
-        ParameterValidator.required(params, 'Docker', 'steps', true)
+    DockerUtil(Map params) {
+        ParameterValidator.required(params, 'DockerUtil', 'steps', true)
         this.steps = params.steps
     }
 
     /* Can be called in Jenkinsfile like:
      *
      *     def workDir = "${WORKSPACE}/${env.BRANCH_NAME}-${env.BUILD_ID}"
-     *     def mountDir = docker.getHostMountDir(workDir)
+     *     def mountDir = dockerUtil.getHostMountDir(workDir)
      */
     String getHostMountDir(String workDir, String jenkinsContainerName = 'cicd-jenkins-1', String mountDestination = '/var/jenkins_home') {
         String mountSource = ''
@@ -64,7 +64,7 @@ class Docker implements Serializable {
 
     /* Can be called in Jenkinsfile like:
      *
-     *     def dockerVolumesToDeleteCsv = docker.getContainerVolumes("${uniqueName}-database-1")
+     *     def dockerVolumesToDeleteCsv = dockerUtil.getContainerVolumes("${uniqueName}-database-1")
      */
     String getContainerVolumes(String containerName) {
         String containerString = this.steps.sh(
